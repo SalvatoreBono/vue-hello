ed69b12ad7bf865939a975a7ae2169b9e4917f19
SalvatoreBono <salvatore.bono2001@libero.it>
2023-07-19 18:26:59 +0200

    change color

--- js/main.js
+++ js/main.js
@@ -7,6 +7,24 @@ createApp({
         return {
             title: `Benvenuto in Vue`,
             img: `img/vue.png`,
-        }
-    }
+            color: ``,
+            changer: ``,
+        };
+    },
+    methods: {
+        colorChanger() {
+            if (this.color === `text-danger`) {
+                this.color = `text-primary`;
+            } else {
+                this.color = `text-danger`;
+            }
+        },
+        imgChanger() {
+            if (this.changer === `img-custom`) {
+                this.changer = ``;
+            } else {
+                this.changer = `img-custom`
+            }
+        },
+    },
 }).mount('#app');
